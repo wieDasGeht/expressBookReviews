599b65e658ab24108e6e1219bb458ecc895166fd
Wie das geht <107952877+wieDasGeht@users.noreply.github.com>
2024-02-29 22:02:43 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -5,26 +5,31 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
 app.use("/customer/auth/*", function auth(req, res, next) {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    if (!token) return res.status(401).send('Access Denied: No Token Provided!');
-
-    try {
-        const verified = jwt.verify(token, 'fingerprint_customer');
-        req.user = verified;
-        next();
-    } catch (err) {
-        res.status(400).send('Invalid Token');
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken']
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user
+                next()
+            }
+            else {
+                return res.status(403).json({ message: "User not authenticated" })
+            }
+        })
+    } else {
+        return res.status(403).json({ message: "User not logged in" })
     }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log(`Server is running: ${PORT}`));
+app.listen(PORT, () => console.log("Server is running"));
